e964d686444a8f23f7ce7952ece9322c02c05239
jasonminwu <143153601+jasonminwu@users.noreply.github.com>
2026-06-08 16:04:38 +0800

    Refactor subscription with new app handling and metadata

--- src/subscription.ts
+++ src/subscription.ts
@@ -1,16 +1,28 @@
 import { defineGkdSubscription } from '@gkd-kit/define';
 import { batchImportApps } from '@gkd-kit/tools';
 import categories from './categories';
-import globalGroups from './globalGroups';
+import globalGroups, { OPEN_AD_ORDER } from './globalGroups';
+import { RawApp, RawAppGroup } from '@gkd-kit/api';
+
+const apps = await batchImportApps(`${import.meta.dirname}/apps`);
+const rawApps: RawApp[] = [];
+apps.forEach((appConfig) => {
+  appConfig.groups?.forEach((g: RawAppGroup) => {
+    if (g.name.startsWith('开屏广告')) {
+      g.order = OPEN_AD_ORDER;
+    }
+  });
+  rawApps.push(appConfig);
+});
 
 export default defineGkdSubscription({
-  id: 233,
-  name: 'Subscription',
+  id: 2333,
+  name: 'jason的订阅',
   version: 0,
-  author: 'author',
+  author: 'jason',
   checkUpdateUrl: './gkd.version.json5',
-  supportUri: 'https://github.com/gkd-kit/subscription-template',
+  supportUri: 'https://github.com/jasonminwu/gkd_subscription',
   categories,
   globalGroups,
-  apps: await batchImportApps(`${import.meta.dirname}/apps`),
+  apps: rawApps,
 });
